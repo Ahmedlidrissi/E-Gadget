8253b6b3f2236b61c9e0dd9588eea640504257f1
Ahmedlidrissi <lidrissiahmed461@gmail.com>
2024-05-25 20:16:56 +0100

    h

---
+++ backend/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('products', ProductController::class);
+
+
+
+
+// rcategorie routs
+Route::get('/categories', CategorieController::class .'@index')->name('categories.index');
+Route::post('/categories', CategorieController::class .'@store')->name('categories.store');
+Route::get('/categories/{categorie}', CategorieController::class.'@show')->name('categories.show');
+Route::put('/categories/{categories}', CategorieController::class .'@update')->name('categories.update');
+Route::delete('/categories/{categories}', CategorieController::class .'@destroy')->name('categories.destroy');
+// rcategorie routs
